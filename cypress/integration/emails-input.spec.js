cff88f07474a6ee20eb8356760ecb864ed72d9b7
Ilia Barchugov <ilya.barchugov@ubsend.com>
2020-04-03 22:54:06 +0200

    add e2e tests

---
+++ cypress/integration/emails-input.spec.js
@@ -0,0 +1,50 @@
+
+describe('EmailsInputComponent', () => {
+  before(() => {
+    // arrange
+    cy.visit('index.html');
+  })
+
+  it('should focus the input when clicking on the email input component', () => {
+    // act
+    cy.get('.emails-input').click();
+
+    // assert
+    cy.get('.emails-input__input').should('be.focused');
+  })
+
+  it('should add entity on ENTER and COMMA key', () => {
+    // act
+    cy.get('.emails-input__input').type('Hello World{enter}jason.bourne@mail.com,foma.kiniaev@aol.com,');
+
+    // assert
+    cy.get('.emails-input__entity:first-child').should('contain.text', 'Hello World');
+    cy.get('.emails-input__entity:nth-child(2)').should('contain.text', 'jason.bourne@mail.com');
+    cy.get('.emails-input__entity:nth-child(3)').should('contain.text', 'foma.kiniaev@aol.com');
+  })
+
+  it('should remove entity on BACKSPACE key', () => {
+    // act
+    cy.get('.emails-input__input').type('{backspace}{backspace}{backspace}');
+
+    // assert
+    cy.get('.emails-input__entity').should('not.exist');
+  })
+
+  it('should distinguish valid and invalid emails', () => {
+    // act
+    cy.get('.emails-input__input').type('Hello World{enter}foma.kiniaev@gmail.com{enter}');
+
+    // assert
+    cy.get('.emails-input__entity:first-child').should('have.class', 'is-invalid');
+    cy.get('.emails-input__entity:nth-child(2)').should('not.have.class', 'is-invalid');
+  })
+
+  it('should remove entity on clicking remove entity button', () => {
+    // act
+    cy.get('.emails-input__entity-btn').click({ multiple: true });
+
+    // assert
+    cy.get('.emails-input__entity').should('not.exist');
+  })
+})
