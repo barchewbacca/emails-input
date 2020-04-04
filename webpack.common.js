5a9d36eef365e3f6cd1575bdaa5787e68e76497a
Ilia Barchugov <ilya.barchugov@ubsend.com>
2020-04-04 10:48:16 +0200

    omit perf warning

--- webpack.common.js
+++ webpack.common.js
@@ -4,24 +4,23 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   entry: path.resolve(__dirname, 'src'),
-
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
-
   resolve: {
     extensions: ['.ts', '.tsx', '.js', '.json']
   },
-
   module: {
     rules: [
       { test: /\.(ts|js)x?$/, loader: 'babel-loader', exclude: /node_modules/ },
     ],
   },
-
   plugins: [
     new CleanWebpackPlugin(),
     new ForkTsCheckerWebpackPlugin(),
-  ]
+  ],
+  performance: {
+    hints: false
+  }
 };
